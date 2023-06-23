a268805abaeb63d4f0e0b374634392a89efbaf4d
Anton Khozyainov <alk3223232@yandex.ru>
2023-06-23 19:06:16 +0300

    Done

--- app/src/main/java/ru/khozyainov/homework1/MainActivity.kt
+++ app/src/main/java/ru/khozyainov/homework1/MainActivity.kt
@@ -1,11 +1,43 @@
 package ru.khozyainov.homework1
 
+import android.content.ComponentName
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.common.util.concurrent.MoreExecutors
+import ru.khozyainov.homework1.databinding.ActivityMainBinding
 
+@UnstableApi
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var controllerFuture: ListenableFuture<MediaController>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        val sessionToken = SessionToken(this, ComponentName(this, MusicPlayerService::class.java))
+        controllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
+
+        controllerFuture.addListener({
+            binding.playerControlView.player = controllerFuture.get()
+        }, MoreExecutors.directExecutor())
+    }
+
+    override fun onStop() {
+        super.onStop()
+        MediaController.releaseFuture(controllerFuture)
+    }
+
 }
